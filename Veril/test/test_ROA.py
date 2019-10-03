4faa175b28895535bd25ed45f9d3634e6230ba43
Shen Shen <shenshen@mit.edu>
2019-10-03 18:08:38 -0400

    past test for VDP findL1

--- Veril/test/test_ROA.py
+++ Veril/test/test_ROA.py
@@ -11,44 +11,48 @@ from pydrake.all import (MathematicalProgram, Polynomial, SolutionResult,
 from scipy.linalg import solve_lyapunov, solve_discrete_lyapunov
 from Veril.Verifier import *
 
-options = opt(1)
+options = opt(2,6,6,4)
 
 prog = MathematicalProgram()
 # add the constant basis
 # constant = prog.NewIndeterminates(1, 'constant')
 # basis = [sym.Monomial(constant[0], 0)]
+
+
 # Cubic testing
-x = prog.NewIndeterminates(1, "x")
-xdot = [-x[0]+x[0]**3]
+# x = prog.NewIndeterminates(1, "x")
+# xdot = [-x[0]+x[0]**3]
 
-# print('here the jacobian')
-# print(J)
-env = dict(zip(x,np.zeros(x.shape)))
-# print(J.Evaluate(env))
-# numpy_compare.assert_equal(e.Substitute(env), x + y + 5)
-A = np.reshape(np.array([-1]),(1,1))
-# print(solve_lyapunov(A,-np.eye(1)))
-S0 = np.reshape(np.array([0.5]),(1,1))
-# x = np.reshape(np.array([x]),(1,1))
-V0 = (x.T@S0@x)
-
-
-
-# vdp testing
-# x = prog.NewIndeterminates(2, "x")
-# xdot = [x(1,); -x(0,)-x(1,:)*(x(0,)**2-1)];
 # # print('here the jacobian')
 # # print(J)
 # env = dict(zip(x,np.zeros(x.shape)))
-# J=Jacobian(xdot,x)
-
-# print(J.Evaluate(env))
+# # print(J.Evaluate(env))
 # # numpy_compare.assert_equal(e.Substitute(env), x + y + 5)
 # A = np.reshape(np.array([-1]),(1,1))
 # # print(solve_lyapunov(A,-np.eye(1)))
 # S0 = np.reshape(np.array([0.5]),(1,1))
 # # x = np.reshape(np.array([x]),(1,1))
 # V0 = (x.T@S0@x)
+
+
+
+# vdp testing
+x = prog.NewIndeterminates(2, "x")
+xdot = -np.array([x[1],-x[0]-x[1]*(x[0]**2-1)])
+# # print('here the jacobian')
+# # print(J)
+J=Jacobian(xdot,x)
+env = dict(zip(x,np.zeros(x.shape)))
+A = np.array([[i.Evaluate(env) for i in j]for j in J])
+print(A)
+# print(J.Evaluate(env))
+# # numpy_compare.assert_equal(e.Substitute(env), x + y + 5)
+# A = np.reshape(np.array([-1]),(1,1))
+# # print(solve_lyapunov(A,-np.eye(1)))
+S0 = solve_lyapunov(A.T,-np.eye(2))
+print(S0)
+# # x = np.reshape(np.array([x]),(1,1))
+V0 = (x.T@S0@x)
 
 bilinear(x, V0, xdot, S0, A, options)
 
