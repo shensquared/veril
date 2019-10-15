0b848a516e6b99d28c5f7290412c89ab19e6733b
Shen Shen <shenshen@mit.edu>
2019-10-15 12:17:40 -0400

    clean up linear models and trainings

--- Veril/kernel.py
+++ Veril/kernel.py
@@ -6,14 +6,29 @@ from CustomLayers import *
 import keras
 from keras.utils import CustomObjectScope
 
-# TODO: understand K.dot vs K.batch_dot
+'''
+A note about the DOT layer: if input is 1: (None, a) and 2: (None, a) then no
+need to do transpose, direct Dot()(1,2) and the output works correctly with
+shape (None, 1).
+
+If input is 1: (None, a, b) and 2: (None, b)
+
+'''
 
 def negativity(y_true, y_pred):
     return K.max(y_pred)
 
+def get_data(d=30, num_grid=100):
+    x1 = np.linspace(-d, d, num_grid)
+    x2 = np.linspace(-d, d, num_grid)
+    x1 = x1[np.nonzero(x1)]
+    x2 = x2[np.nonzero(x2)]
+    x1, x2 = np.meshgrid(x1, x2)
+    x1, x2 = x1.ravel(), x2.ravel()
+    return [np.array([x1, x2]).T, np.zeros(x1.shape)]
 
 def linear_model(sys_dim, A):
-    x = Input(shape=(sys_dim,))
+    x = Input(shape=(sys_dim,)) # x shape (None, sys_dim)
     layers = [
         Dense(5, input_shape=(sys_dim,), use_bias=False,
               kernel_regularizer=keras.regularizers.l2(0.)),
@@ -22,19 +37,43 @@ def linear_model(sys_dim, A):
     ]
     layers = layers + [TransLayer(i) for i in layers[::-1]]
 
-    seq_model = Sequential(layers)
-    xLL = seq_model(x)
+    xLL = Sequential(layers)(x)# xLL shape (None, sys_dim)
     # need to avoid 0 in the denominator (by adding a strictly positive scalar
     # to V)
-    V = Dot(1)([xLL, x])
-    Vdot = DotKernel(A)(xLL)
-    Vdot = Dot(1)([Vdot, x])
+    V = Dot(1)([xLL, x]) # V shape (None, 1)
+    xLLA = DotKernel(A)(xLL) #A shape (sys_dim,sys_dim), xLLA: (None, sys_dim)
+    Vdot = Dot(1)([xLLA, x]) #Vdot (None,1)
     rate = Divide()([Vdot, V])
 
     model = Model(inputs=x, outputs=rate)
     model.compile(loss=negativity, optimizer='adam')
     print(model.summary())
     return model
+
+def linear_train():
+    callbacks = []
+    # if write_log:
+    #     logs_dir = "/Users/shenshen/Veril/data/lyapunov"
+    #     tensorboard = TensorBoard(log_dir=logs_dir, histogram_freq=0,
+    #                               write_graph=True, write_images=True)
+    #     callbacks.append(tensorboard)
+    x, y = get_data()
+    A = np.array([[-.1, 0], [1, -2]])
+    model = linear_model(2, A)
+    print(model.predict(x))
+    history = model.fit(x, y, epochs=15, verbose=True, callbacks=callbacks)
+    weights = [K.eval(i) for i in model.weights]
+    print(weights)
+    weights = np.linalg.multi_dot(weights)
+    P = weights@weights.T
+    # print(np.linalg.eig(P)[0]
+    print(np.linalg.eig(P@A + A.T@P)[0])
+    model_file_name = '/Users/shenshen/Veril/data/Kernel/lyap_model.h5'
+    model.save(model_file_name)
+    return P
+    # del model
+    # print("Saved model" + model_file_name + "to disk")
+    # model
 
 
 def polynomial_model(sys_dim, max_deg):
@@ -67,45 +106,6 @@ def polynomial_model(sys_dim, max_deg):
     model.compile(loss=negativity, optimizer='adam')
     print(model.summary())
     return model
-
-
-def get_data(d=30, num_grid=100):
-    x1 = np.linspace(-d, d, num_grid)
-    x2 = np.linspace(-d, d, num_grid)
-    x1 = x1[np.nonzero(x1)]
-    x2 = x2[np.nonzero(x2)]
-    x1, x2 = np.meshgrid(x1, x2)
-    x1, x2 = x1.ravel(), x2.ravel()
-    return [np.array([x1, x2]).T, np.zeros(x1.shape)]
-
-
-def linear_train():
-    callbacks = []
-    # if write_log:
-    #     logs_dir = "/Users/shenshen/Veril/data/lyapunov"
-    #     tensorboard = TensorBoard(log_dir=logs_dir, histogram_freq=0,
-    #                               write_graph=True, write_images=True)
-    #     callbacks.append(tensorboard)
-    x, y = get_data()
-    A = np.array([[-.1, 0], [1, -2]])
-    model = linear_model(2, A)
-
-    print(model.predict(x))
-
-    history = model.fit(x, y, epochs=15, verbose=True, callbacks=callbacks)
-    weights = [K.eval(i) for i in model.weights]
-    print(weights)
-    weights = np.linalg.multi_dot(weights)
-    P = weights@weights.T
-    # print(np.linalg.eig(P)[0]
-    print(np.linalg.eig(P@A + A.T@P)[0])
-    model_file_name = '/Users/shenshen/Veril/data/Kernel/lyap_model.h5'
-    model.save(model_file_name)
-    return P
-    # del model
-    # print("Saved model" + model_file_name + "to disk")
-    # model
-
 
 def VDP(x):
     x1 = (K.dot(x, K.constant([1, 0], shape=(2, 1))))
