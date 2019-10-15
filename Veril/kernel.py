a74a54863b087016b704c6bfff5e7959b8774e8e
Shen Shen <shenshen@mit.edu>
2019-10-15 13:04:05 -0400

    poly_train works (if fed with stable samples), cleaned up

--- Veril/kernel.py
+++ Veril/kernel.py
@@ -13,10 +13,19 @@ shape (None, 1).
 
 If input is 1: (None, a, b) and 2: (None, b)
 
+If debug, use kernel_initializer= keras.initializers.Ones()
+
+    # if write_log:
+    #     logs_dir = "/Users/shenshen/Veril/data/lyapunov"
+    #     tensorboard = TensorBoard(log_dir=logs_dir, histogram_freq=0,
+    #                               write_graph=True, write_images=True)
+    #     callbacks.append(tensorboard)
 '''
+
 
 def negativity(y_true, y_pred):
     return K.max(y_pred)
+
 
 def get_data(d=30, num_grid=100):
     x1 = np.linspace(-d, d, num_grid)
@@ -27,8 +36,9 @@ def get_data(d=30, num_grid=100):
     x1, x2 = x1.ravel(), x2.ravel()
     return [np.array([x1, x2]).T, np.zeros(x1.shape)]
 
+
 def linear_model(sys_dim, A):
-    x = Input(shape=(sys_dim,)) # x shape (None, sys_dim)
+    x = Input(shape=(sys_dim,))  # x: (None, sys_dim)
     layers = [
         Dense(5, input_shape=(sys_dim,), use_bias=False,
               kernel_regularizer=keras.regularizers.l2(0.)),
@@ -37,12 +47,12 @@ def linear_model(sys_dim, A):
     ]
     layers = layers + [TransLayer(i) for i in layers[::-1]]
 
-    xLL = Sequential(layers)(x)# xLL shape (None, sys_dim)
+    xLL = Sequential(layers)(x)  # xLL shape (None, sys_dim)
     # need to avoid 0 in the denominator (by adding a strictly positive scalar
     # to V)
-    V = Dot(1)([xLL, x]) # V shape (None, 1)
-    xLLA = DotKernel(A)(xLL) #A shape (sys_dim,sys_dim), xLLA: (None, sys_dim)
-    Vdot = Dot(1)([xLLA, x]) #Vdot (None,1)
+    V = Dot(1)([xLL, x])  # V: (None, 1)
+    xLLA = DotKernel(A)(xLL) # A: (sys_dim,sys_dim), xLLA: (None, sys_dim)
+    Vdot = Dot(1)([xLLA, x])  # Vdot: (None,1)
     rate = Divide()([Vdot, V])
 
     model = Model(inputs=x, outputs=rate)
@@ -50,101 +60,74 @@ def linear_model(sys_dim, A):
     print(model.summary())
     return model
 
+
 def linear_train():
     callbacks = []
-    # if write_log:
-    #     logs_dir = "/Users/shenshen/Veril/data/lyapunov"
-    #     tensorboard = TensorBoard(log_dir=logs_dir, histogram_freq=0,
-    #                               write_graph=True, write_images=True)
-    #     callbacks.append(tensorboard)
     x, y = get_data()
     A = np.array([[-.1, 0], [1, -2]])
     model = linear_model(2, A)
     print(model.predict(x))
     history = model.fit(x, y, epochs=15, verbose=True, callbacks=callbacks)
+    # getting the weights and verify the eigs
     weights = [K.eval(i) for i in model.weights]
-    print(weights)
     weights = np.linalg.multi_dot(weights)
     P = weights@weights.T
-    # print(np.linalg.eig(P)[0]
-    print(np.linalg.eig(P@A + A.T@P)[0])
+    print('eig of orignal PA+A\'P  %s' % (np.linalg.eig(P@A + A.T@P)[0]))
     model_file_name = '/Users/shenshen/Veril/data/Kernel/lyap_model.h5'
     model.save(model_file_name)
+    del model
+    print("Saved model" + model_file_name + "to disk")
     return P
-    # del model
-    # print("Saved model" + model_file_name + "to disk")
-    # model
 
 
-def polynomial_model(sys_dim, max_deg):
+def poly_model(sys_dim, max_deg=4):
     # constant = Input(shape=(1,))
-    x = Input(shape=(sys_dim,))
-    phi = Polynomials(max_deg)(x)
+    x = Input(shape=(sys_dim,))  # x: (None, sys_dim)
+    phi = Polynomials(max_deg)(x)  # phi: (None, monomial_dim)
     # phi = Concatenate()([constant,phi])
 
     layers = [
-        Dense(1, use_bias=False, kernel_regularizer=keras.regularizers.l2(0.)),
-        Dense(1, use_bias=False, kernel_regularizer=keras.regularizers.l2(0.)),
-        Dense(1, use_bias=False, kernel_regularizer=keras.regularizers.l2(0.))
+        Dense(10, use_bias=False, kernel_regularizer=keras.regularizers.l2(0.)),
+        Dense(5, use_bias=False, kernel_regularizer=keras.regularizers.l2(0.)),
+        Dense(10, use_bias=False, kernel_regularizer=keras.regularizers.l2(0.))
     ]
     layers = layers + [TransLayer(i) for i in layers[::-1]]
-    seq_model = Sequential(layers)
-    phiLL = seq_model(phi)
+    phiLL = Sequential(layers)(phi)  # phiLL: (None, monomial_dim)
+
     # # need to avoid 0 in the denominator (by adding a strictly positive scalar
     # # to V)
-    # V = Dot(1)([phiLL, phiLL])
-    dphidx = DiffPoly(max_deg)(x)
-    fx = Permute((1,2))(Lambda(VDP, Poly_dynamics_shape)(x))
-    # dVdx = Lambda(threeDdot,threeDdot_shape)([phiLL,dphidx])
-    dVdx = Dot(1)([phiLL,dphidx])
-    Vdot = Dot(1)([dVdx, fx])
-    # dVdx = SimpleDot(-1)([dphidx,phiLL])
-    # Vdot = Lambda(threeDdot,threeDdot_shape)([dVdx,fx])
+    V = Dot(1)([phiLL, phi])  # V: (None,1)
+    dphidx = DiffPoly(max_deg)(x)  # dphidx: (None, monomial_dim, sys_dm)
+    phiLLdphidx = Dot(1)([phiLL, dphidx])  # (None, sys_dim)
+    fx = Lambda(VDP, Poly_dynamics_shape)(x) # (None, sys_dim)
+    Vdot = Dot(1)([phiLLdphidx, fx])  # (None, 1)
 
-    # rate = Divide()([Vdot, dVdx])
-    model = Model(inputs=x, outputs=Vdot)
+    rate = Divide()([Vdot, V])
+    model = Model(inputs=x, outputs=rate)
     model.compile(loss=negativity, optimizer='adam')
     print(model.summary())
     return model
+
 
 def VDP(x):
     x1 = (K.dot(x, K.constant([1, 0], shape=(2, 1))))
     x2 = (K.dot(x, K.constant([0, 1], shape=(2, 1))))
     dx = (K.concatenate([-x2, -(1 - x1**2) * x2 + x1]))
-    return K.expand_dims(dx)
+    return dx
+
 
 def Poly_dynamics_shape(input_shapes):
-    input_shape = list(input_shapes)
-    input_shape.append(1)
-    output_shapes = tuple(input_shape)
-    return output_shapes
+    return input_shapes
 
-def threeDdot(x):
-    # TODO: write for more than 2 input list
-    x1,x2=x
-    return K.dot(x1,x2)
-
-def threeDdot_shape(input_shapes):
-
-    shape1=list(input_shapes[0])
-    shape2=list(input_shapes[1])
-    # print(shape1)
-    # print(shape2)
-    shape1.pop(1)
-    shape2.pop(1)
-    shape2.pop(0)
-    output_shapes = shape1 + shape2
-    # print(output_shapes)
-    return tuple(output_shapes)
 
 def poly_train():
     callbacks = []
-    x, y = get_data(d=1.5,num_grid=2)
-    # print(x.shape, y.shape)
-    model = polynomial_model(2,3)
-    print(model.predict(x))
-    # history = model.fit(x, y, epochs=15, verbose=True,
-    # callbacks=callbacks)
+    sys_dim =2
+    x, y = get_data(d=1)
+    model = poly_model(sys_dim, max_deg = 3)
+    # print(model.predict(x))
+    history = model.fit(x, y, epochs=150, verbose=True,
+                        callbacks=callbacks)
     # weights = [K.eval(i) for i in model.weights]
     # print(weights)
     # weights = np.linalg.multi_dot(weights)
@@ -154,5 +137,4 @@ def poly_train():
     # model_file_name = '/Users/shenshen/Veril/data/Kernel/lyap_model.h5'
     # model.save(model_file_name)
     # return P
-
 poly_train()
