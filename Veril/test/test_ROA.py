fe355743c5d3ec87cd47c1cae0294ddf3edb4b26
Shen Shen <shenshen@mit.edu>
2019-10-16 16:51:13 -0400

    clean up level-set method

--- Veril/test/test_ROA.py
+++ Veril/test/test_ROA.py
@@ -36,41 +36,28 @@ V0 = (x.T@S0@x)
 bilinear(V0, xdot, S0, A, options)
 
 
-def levelsetMethod(old_x, f, V, options):
+def levelsetMethod(x, f, V, options):
     prog = MathematicalProgram()
-    x = prog.NewIndeterminates(options.nX, 'lx')
-    V = V.Substitute(dict(zip(old_x, x)))
-    f = [i.Substitute(dict(zip(old_x, x))) for i in f]
+    prog.AddIndeterminates(x)
     # % construct multipliers for Vdot
     L1 = prog.NewFreePolynomial(Variables(x), options.degL1).ToExpression()
-    # L1 = L1[0].ToExpression()
-    print(L1)
     # % construct Vdot
-    # x = list(V.GetVariables())
-    Vdot = (V.Jacobian(x) @ f)
-    # print('Vdot')
-    # print(Vdot)
+    Vdot = clean(V.Jacobian(x) @ f)
     # % construct slack var
     sigma1 = prog.NewContinuousVariables(1, "s")[0]
-    # prog.AddConstraint(sigma1 >= 0)
-    # % setup SOS constraints
-    # print(Polynomial(-Vdot + L1 * (V - 1) - sigma1 * V).TotalDegree())
-    # prog.AddSosConstraint(-Vdot + L1 * (V - 1) - sigma1 * V)
-    print('v')
-    print(V)
-    print('vdot')
-    print(Vdot)
     prog.AddSosConstraint((x.T@x)**4 * (V - sigma1) + L1 * Vdot)
     # add cost
     prog.AddCost(-sigma1)
-    result = Solve(prog)
-    print('w/ solver %s' % (result.get_solver_id().name()))
-    print(result.get_solution_result())
+
+    solver = MosekSolver()
+    solver.set_stream_logging(False, "")
+    result = solver.Solve(prog, None, None)
+    # print('w/ solver %s' % (result.get_solver_id().name()))
     assert result.is_success()
     L1 = result.GetSolution(L1)
     sigma1 = result.GetSolution(sigma1)
     print(sigma1)
-    return L1, sigma1
+    return sigma1
 
 
 # def findL1(old_x, f, V, options):
