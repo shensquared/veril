15a56156f36e0226fc38b498310e7a55effa5c69
Shen Shen <shenshen@mit.edu>
2019-11-16 01:46:21 -0500

    swap the order of scatter and ROA boundary (to check for overflow)

--- Veril/util/plotFunnel.py
+++ Veril/util/plotFunnel.py
@@ -4,9 +4,8 @@ import matplotlib.pyplot as plt
 from util import samples
 
 def plotFunnel(V):
-  xlim = np.load('../data/VDP/VanDerPol_limitCycle.npy')
-  fig, ax = plt.subplots()
-  bdry = ax.plot(xlim[0, :], xlim[1, :], label='ROA boundary')
-  x = samples.levelsetData(V)[0]
-  plt.fill(x[:,0], x[:,1])
-  plt.show()
+    x = samples.levelsetData(V)[0]
+    plt.fill(x[:,0], x[:,1])
+    xlim = np.load('../data/VDP/VanDerPol_limitCycle.npy')
+    bdry = plt.plot(xlim[0, :], xlim[1, :],color='red', label='ROA boundary')
+    plt.show()
