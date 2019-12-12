2d1c58e88735656c268eacf10e60db39e7751311
Shen Shen <shenshen@mit.edu>
2019-12-12 12:41:04 -0500

    consistent naming

--- Veril/util/plots.py
+++ Veril/util/plots.py
@@ -8,7 +8,7 @@ def plot_funnel(V, sys_name):
     x = samples.levelsetData(V)[0]
     plt.fill(x[:, 0], x[:, 1])
     if sys_name is 'VanderPol':
-        xlim = np.load('../data/VanderPol/VanDerPol_limitCycle.npy')
+        xlim = np.load('../data/VanderPol/VanderPol_limitCycle.npy')
         bdry = plt.plot(xlim[0, :], xlim[1, :],
                         color='red', label='ROA boundary')
     plt.show()
@@ -17,7 +17,7 @@ def plot_funnel(V, sys_name):
 def scatterSamples(samples, sys_name):
     plt.scatter(samples[:, 0], samples[:, 1])
     if sys_name is 'VanderPol':
-        xlim = np.load('../data/VanderPol/VanDerPol_limitCycle.npy')
+        xlim = np.load('../data/VanderPol/VanderPol_limitCycle.npy')
         bdry = plt.plot(xlim[0, :], xlim[1, :],
                         color='red', label='ROA boundary')
     plt.show()
