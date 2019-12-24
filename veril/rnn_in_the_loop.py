645985acb55e7736c884dd39a9dad23017668363
Shen Shen <shenshen@mit.edu>
2019-12-24 17:36:16 -0500

    separation of closed-loop and rnn

--- veril/rnn_in_the_loop.py
+++ veril/rnn_in_the_loop.py
@@ -6,6 +6,76 @@ from veril import plants
 from veril.custom_layers import JanetController
 
 
+options = {
+    'plant_name': 'DoubleIntegrator',
+    # 'plant_name': 'DubinsPoly',
+    # 'plant_name': 'DubinsTrig',
+    # 'plant_name': 'Pendulum',
+    # 'plant_name': 'Satellite',
+    'num_units': 4,
+    'timesteps': 1000,
+    'num_samples': 10000,
+    'batch_size': 1,
+    'epochs': 10,
+    'dt': 1e-3,
+    'obs_idx': None,
+    'tag': '',
+}
+
+
+def sim_RNN_stable_samples(**options):
+    old_sampels = np.load('DIsamples.npy')
+    model, model_file_name = closed_loop.get_NNorCL(NNorCL='NN', **options)
+    samples = closed_loop.sample_stable_inits(
+        model, 20000, 1000, lb=-1.5, ub=1.5)
+    np.save('DIsamples', np.vstack([old_sampels, samples]))
+
+
+def train_RNN_controller(pre_trained=None, **kwargs):
+    num_samples = kwargs.pop('num_samples')
+    num_units = kwargs.pop(
+        'num_units')
+    timesteps = kwargs.pop('timesteps')
+    dt = kwargs.pop('dt')
+    obs_idx = kwargs.pop('obs_idx')
+    tag = kwargs.pop('tag')
+    plant_name = kwargs.pop('plant_name')
+
+    plant = plants.get(plant_name, dt, obs_idx)
+    if pre_trained is None:
+        [init_x, init_c, ext_in] = [
+            Input(shape=(plant.num_states,), name='init_x'),
+            Input(shape=(num_units,), name='init_c'),
+            Input(shape=(None, None), name='ext_in')
+        ]
+        Janet_layer = JanetController(
+            num_units, plant_name=plant_name, dt=dt,
+            obs_idx=obs_idx)
+        out = Janet_layer(ext_in, initial_state=[init_x, init_c])
+        model = Model([init_x, init_c, ext_in], out)
+        model.compile(optimizer='adam', loss='mse', metrics=['mse'])
+        print(model.summary())
+    else:
+        model = pre_trained
+
+    dirname = os.path.dirname(__file__) + '../data/'
+    model_file_name = dirname + plant.name + '/' + \
+        'unit' + str(num_units) + 'step' + str(timesteps) + tag
+
+    callbacks = [ModelCheckpoint(model_file_name + '.h5',
+                                 monitor='val_loss', verbose=0,
+                                 save_best_only=False,
+                                 save_weights_only=False, mode='auto',
+                                 period=1)]
+    print('Train...')
+    [x_train, y_train] = plant.get_data(num_samples, timesteps, num_units)
+    history = model.fit(x_train, y_train, callbacks=callbacks, **kwargs)
+    last_loss = history.history['loss'][-1]
+    model_file_name = model_file_name + 'loss' + str(last_loss) + tag + '.h5'
+    model.save(model_file_name)
+    print("Saved model " + model_file_name + " to disk")
+
+
 def get_NNorCL(NNorCL='CL', **kwargs):
     num_samples = kwargs['num_samples']
     num_units = kwargs['num_units']
--- veril/run.py
+++ veril/run.py
@@ -3,83 +3,16 @@ import argparse
 
 from keras.models import Model, load_model
 from keras.layers import Input
-from keras.callbacks import ModelCheckpoint
-
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 # from keras import regularizers
 
 import h5py
 import numpy as np
 
-from veril import plants, symbolic_verifier, closed_loop, sample_lyap, sample_variety
-from veril.custom_layers import JanetController
+from veril import symbolic_verifier, closed_loop, sample_lyap, sample_variety
 from veril.util.plots import *
-
 # from util.samples import withinLevelSet
 import time
-
-
-options = {
-    'plant_name': 'DoubleIntegrator',
-    # 'plant_name': 'DubinsPoly',
-    # 'plant_name': 'DubinsTrig',
-    # 'plant_name': 'Pendulum',
-    # 'plant_name': 'Satellite',
-    'num_units': 4,
-    'timesteps': 1000,
-    'num_samples': 10000,
-    'batch_size': 1,
-    'epochs': 10,
-    'dt': 1e-3,
-    'obs_idx': None,
-    'tag': '',
-}
-
-
-def train_RNN_controller(pre_trained=None, **kwargs):
-    num_samples = kwargs.pop('num_samples')
-    num_units = kwargs.pop(
-        'num_units')
-    timesteps = kwargs.pop('timesteps')
-    dt = kwargs.pop('dt')
-    obs_idx = kwargs.pop('obs_idx')
-    tag = kwargs.pop('tag')
-    plant_name = kwargs.pop('plant_name')
-
-    plant = plants.get(plant_name, dt, obs_idx)
-    if pre_trained is None:
-        [init_x, init_c, ext_in] = [
-            Input(shape=(plant.num_states,), name='init_x'),
-            Input(shape=(num_units,), name='init_c'),
-            Input(shape=(None, None), name='ext_in')
-        ]
-        Janet_layer = JanetController(
-            num_units, plant_name=plant_name, dt=dt,
-            obs_idx=obs_idx)
-        out = Janet_layer(ext_in, initial_state=[init_x, init_c])
-        model = Model([init_x, init_c, ext_in], out)
-        model.compile(optimizer='adam', loss='mse', metrics=['mse'])
-        print(model.summary())
-    else:
-        model = pre_trained
-
-    dirname = os.path.dirname(__file__)
-    dirname = dirname + '../data/'
-    # dirname = os.path.join('/users/shenshen/veril/data/')
-    model_file_name = dirname + plant.name + '/' + \
-        'unit' + str(num_units) + 'step' + str(timesteps) + tag
-
-    callbacks = [ModelCheckpoint(model_file_name + '.h5',
-                                 monitor='val_loss', verbose=0,
-                                 save_best_only=False,
-                                 save_weights_only=False, mode='auto',
-                                 period=1)]
-    print('Train...')
-    [x_train, y_train] = plant.get_data(num_samples, timesteps, num_units)
-    history = model.fit(x_train, y_train, callbacks=callbacks, **kwargs)
-    last_loss = history.history['loss'][-1]
-    model_file_name = model_file_name + 'loss' + str(last_loss) + tag + '.h5'
-    model.save(model_file_name)
-    print("Saved model " + model_file_name + " to disk")
 
 
 def train_V(sys_name, max_deg=3, epochs=15, method='SGD'):
@@ -99,12 +32,15 @@ def train_V(sys_name, max_deg=3, epochs=15, method='SGD'):
         [phi, dphidx, f] = system.train_for_V_features(train_x)
 
     if method is 'SGD':
-        y = - np.ones(phi.shape)
+        num_samples = phi.shape[0]
+        y = - np.ones((num_samples,))
         if model is None:
             model = sample_lyap.poly_model_for_V(system.num_states, max_deg)
         history = model.fit([phi, dphidx, f], y, epochs=epochs, verbose=True)
         # assert (history.history['loss'][-1] <= 0)
         P = sample_lyap.get_gram_for_V(model)
+        # yy = model.predict([phi, dphidx, f])
+        # evals = model.evaluate([phi, dphidx, f], y, verbose=True)
     else:
         P = symbolic_verifier.convexly_search_for_V_on_samples(phi, dphidx, f)
     V, Vdot = system.rescale_V(P, train_x)
@@ -166,19 +102,11 @@ def verify_via_bilinear(sys_name, max_deg=3):
     return system, V
 
 
-def sim_RNN_stable_samples(**options):
-    old_sampels = np.load('DIsamples.npy')
-    model, model_file_name = closed_loop.get_NNorCL(NNorCL='NN', **options)
-    samples = closed_loop.sample_stable_inits(
-        model, 20000, 1000, lb=-1.5, ub=1.5)
-    np.save('DIsamples', np.vstack([old_sampels, samples]))
-
-
 # system, V = verify_via_bilinear('VanderPol')
 # system, V = verify_via_bilinear('Pendubot',max_deg = 3)
-# verify_via_variety('Pendubot', init_root_threads=120, epochs=100)
-# for i in range(30):
-#   verify_via_variety( 'VanderPol', init_root_threads=30, epochs=20)
+# verify_via_variety('Pendubot', init_root_threads=120, epochs=12)
+for i in range(30):
+    verify_via_variety('VanderPol', init_root_threads=30, epochs=100)
 # V, Vdot, system = train_V('VanderPol', epochs=40)
 #     verify_via_equality(system, V)
 
