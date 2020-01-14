ad8c80c5cfd0ede85abf05049a0773c5282e6b60
Shen Shen <shenshen@mit.edu>
2020-01-14 18:26:33 -0500

    remove scatter plots from the closed-loop

--- veril/closed_loop.py
+++ veril/closed_loop.py
@@ -13,8 +13,6 @@ from numpy.linalg import eig, inv
 from scipy.linalg import solve_lyapunov, solve_discrete_lyapunov
 from scipy import integrate
 
-from veril.util.plots import *
-
 
 def get(system_name):
     if isinstance(system_name, six.string_types):
@@ -187,19 +185,11 @@ class ClosedLoopSys(object):
                 str(i[0] + 1) + str(i[1] + 1) + '.npy'
             np.save(name, samples)
 
-    def scatter_stable_samples(self, slice_idx=None):
-        if slice_idx is None:
-            [scatterSamples(np.zeros((1, self.num_states)), self.name, i)
-             for i in self.all_slices]
-        else:
-            scatterSamples(np.zeros((1, self.num_states)),
-                           self.name, slice_idx)
-
     def get_x(self, **kwargs):
         if 'd' in kwargs:
             d = kwargs['d']
         else:
-            d=self.d
+            d = self.d
         if 'num_grid' in kwargs:
             num_grid = kwargs['num_grid']
         else:
@@ -278,7 +268,7 @@ class Pendubot(ClosedLoopSys):
         if 'd' in kwargs:
             d = kwargs['d']
         else:
-            d=self.d
+            d = self.d
         if 'num_grid' in kwargs:
             num_grid = kwargs['num_grid']
         else:
